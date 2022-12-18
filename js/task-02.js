1d8422b09a9341f68741cc89baa5340837e2ca7d
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2022-12-18 18:56:39 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsList = document.querySelector("ul#ingredients");
+
+const ingredientEls = ingredients.map(ingredient => {
+	const ingredientEl = document.createElement("li");
+	ingredientEl.textContent = ingredient;
+	ingredientEl.className = "item";
+	return ingredientEl;
+});
+
+ingredientsList.append(...ingredientEls);

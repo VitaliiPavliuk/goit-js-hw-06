44b82a0441f9e79de7c46df76bb246788dbd7c1f
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2022-12-18 23:24:53 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const changeColorBtn = document.querySelector("button.change-color");
+const colorEl = document.querySelector("span.color");
+
+changeColorBtn.addEventListener("click", () => {
+	const randomColor = getRandomHexColor();
+	bodyEl.style.backgroundColor = randomColor;
+	colorEl.textContent = randomColor;
+});

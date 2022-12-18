5234369106a0e4f919e9b850276f7902c68f1545
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2022-12-18 19:32:35 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,4 @@
-// const categoriesEl = document.querySelector("#categories");
 const itemEls = document.querySelectorAll(".item");
-// const categoriesTitleEls = document.querySelectorAll(".item h2");
 
 console.log("Number of categories: ", itemEls.length);
 
@@ -8,5 +6,3 @@ itemEls.forEach(item => {
 	console.log("Category: ", item.querySelector("h2").textContent);
 	console.log("Elements: ", item.querySelectorAll("li").length);
 });
-
-// console.log(categoriesTitleEls);

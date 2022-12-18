c16f0e79d789e4ada68837b72357f4a0f011d3a2
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2022-12-18 23:48:48 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector("form.login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		return alert("Please fill in all the fields!");
+	}
+	const user = {};
+	user.email = email.value;
+	user.password = password.value;
+	console.log(user);
+	event.currentTarget.reset();
+}
